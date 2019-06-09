cf2444ceb66119c3e5ada7dba0e7c902ef02c36d
Shawn Pringle <shawn.pringle@gmail.com>
2019-06-08 17:04:03 -0700

    Corrected setup.py to allow uploading to test packaging site

--- python/setup.py
+++ python/setup.py
@@ -9,12 +9,7 @@ setuptools.setup(
     author="Shawn.Pringle",
     author_email="shawn.pringle@gmail.com",
     description="Python Qt5 Classes that Display and Parse Numbers using Standard Decimal Notation",
-    long_description="""These classes and later perhaps routines for purely functional languages are for working with numbers expressed in standard decimal notation.
-
-Often libraries for working with locales neglect to put thousand separators (commas) after the decimal point or they sometimes use scientific notation. In the classes and routines of this package you are guaranteed to always get standard notation and separators after the decimal. This means that commas come before and after the decimal place. Using KDE's settings you can change the comma or decimal to any symbol you wish to use.
-
-This library requires PyQt5 and Python 3.6-3.7.""",
-    long_description_content_type="text/python",
+    long_description=long_description,
     url="https://github.com/shawnpringle/separatorsafterdecimal",
     packages=setuptools.find_packages(),
     classifiers=[
